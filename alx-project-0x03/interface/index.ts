2e4d73dc6c0bb11fde527f0c8b5d7e75eaa629d1
MudMoh <mudesir@gmail.com>
2025-09-28 01:35:54 +0300

    Move your interfaces to the right directory

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,18 @@
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+import { ReactNode } from "react";
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+
